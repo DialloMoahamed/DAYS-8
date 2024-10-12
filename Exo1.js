406d904d0def9b8de1671999db7d17458c27fde2
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-12 15:35:01 +0100

    Question 3 terminer:Ajoutez les propriétés nom, pattes, couleur, âge et aboiement pour l'objet chien. La propriété aboiement est une méthode qui renvoie woof woof

--- Exo1.js
+++ Exo1.js
@@ -4,3 +4,13 @@ let chien = {};
 
 // Reponse numero 2
 console.log(chien);
+
+
+// Reponse numero 3
+chien = {
+    name: 'Rex',
+    legs: 'white',
+    color: 'Black',
+    age : '6 months',
+    bark : "woof woof"
+}
