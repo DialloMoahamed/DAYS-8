bf5a18c70f0c6bc22fdc17d9676120a9727a9c11
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-12 15:41:33 +0100

    Question 4 terminer:Obtenez le nom, les pattes, la couleur, l'âge et la valeur de l'aboiement de l'objet chien

--- Exo1.js
+++ Exo1.js
@@ -14,3 +14,11 @@ chien = {
     age : '6 months',
     bark : "woof woof"
 }
+
+
+// Reponse numero 4
+console.log(chien.name);
+console.log(chien['legs']);
+console.log(chien.color);
+console.log(chien['age']);
+console.log(chien.bark);
