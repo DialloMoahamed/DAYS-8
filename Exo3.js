b36bab142a457ca54a383c3ab471f4399a63fbed
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2024-10-23 14:39:46 +0100

    Question 1 terminer

---
+++ Exo3.js
@@ -0,0 +1,13 @@
+  // Reponse numero 1
+  const personAccount = {
+    firstName: "Mohamed",
+    lastName: "Attahir",
+    incomes: 50000,
+    expenses: 4000,
+    totalIncome: 60000,
+    totalExpense: 35000,
+    accountInfo: 'Compt courant',
+    addIncome: 12000,
+    addExpense: 50000,
+    accountBalance: 30000
+}
